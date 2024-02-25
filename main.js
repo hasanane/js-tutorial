452d62b9ba8cf6de365e72482f94a84876b3610a
Hasanain Sodani <hasananesu22@gmail.com>
2024-02-26 00:15:55 +0330

    in

---
+++ main.js
@@ -0,0 +1,10 @@
+x = "hello world"
+y = `2+2=${7/2}`
+firstName = "hassoni"
+lastName = "sodani"
+a= `${firstName} AL${lastName} ${firstName}`
+// function fullName(first,last){
+//     return `${first} AL${last}`
+// }
+fullName = (first, last) => {return `${first} ${last}`}
+console.log(fullName(firstName,lastName))
