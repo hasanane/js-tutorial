f1d3bee6b86b45eee7ee9dff037356ebf2766cc2
Hasanain Sodani <hasananesu22@gmail.com>
2024-03-03 20:14:19 +0330

    im happy leared prmitive types

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 x = "Hello World"
 y = `2+2=${7/2}`
+// variable types:
+// these are prmitive types
+// 1. number
+num = 1
+// 2. string 
+str = "string"
+// 3. array
+// note: in java script we can mix types but in c we cant
+arr = ["1",num,str]
+// 4. object
+obj = {
+    firstname: "hasooni",
+    age: num,
+    lastname: "soodani",
+    hobbies: ["manga","anime","foot ball","programing"]
+}
+arr[1]
+obj.firstname
+obj["firstname"]
+// 5. boolean
+hungry = false
+sleepy = true
 firstName = "hassoni"
 lastName = "sodani"
 a= `${firstName} AL${lastName} ${firstName}`
