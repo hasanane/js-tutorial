1412be6d01080ecbe2bcd4cf3533268fb0ddea0b
Hasanain Sodani <hasananesu22@gmail.com>
2025-02-04 16:27:52 +0330

    test 2

--- calculator.ts
+++ calculator.ts
@@ -27,7 +27,7 @@ class Calculator {
   #fullEquation: string;
   operations: string[];
   numbers: number[];
-
+  test_1: number;
   add(num: number) {
     this.equals = this.equals + num;
     this.operations[this.operations.length] = "+";
